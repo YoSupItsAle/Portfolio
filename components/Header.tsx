522eac9c76a3f0881c9fd25fb10ceb82c62c4e9e
YoSupItsAle <supitsale@gmail.com>
2024-02-10 15:42:13 +0100

    added Navbar with logo and starting to make themetoggle button

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,16 @@
+import Logo from "./Logo";
+import ThemeToggle from "./ThemeToggle";
+
 function Header() {
-  return <header>
-    <nav>
+  return <header className="sticky top-0 z-50">
+    <nav className="flex flex-col sm:flex-row items-center p-5 pl-2 bg-white dark:bg-gray-900 max-w-7xl mx-auto">
         {/*Logo*/}
+        <Logo />
+        <div className="flex-1 flex items-center justify-end space-x-4">
+          {/*DarkModeToggle*/}
+          <ThemeToggle />
+
+        </div>
     </nav>
   </header>;
 
--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,6 +1,23 @@
+import LogoImage from "@images/LogoImage.png";
+import Link from "next/link";
+import { AspectRatio } from "./ui/aspect-ratio";
+import Image from "next/image";
+
 function Logo() {
-  return 
-    <div>Logo</div>
+  return <Link href="/"  prefetch={false} className="overflow-hidden">
+    <div className="flex item-center w-40 h-14">
+      <AspectRatio 
+      ratio={16 / 9} 
+      className="">
+        <Image
+          priority
+          src={LogoImage}
+          alt="logo"
+          className="dark:filter dark:invert"
+          ></Image>
+      </AspectRatio>
+    </div>
+  </Link>
 }
 
 export default Logo
